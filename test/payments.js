43d314fc235cb5b91b396b2e79beea4408ea72c1
Glenn Geenen <glenn@geenentijd.be>
2016-08-09 18:30:19 +0200

    add test

--- test/payments.js
+++ test/payments.js
@@ -7,6 +7,21 @@ const assert = require('assert');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 
+function validatePayment(payment) {
+  assert(payment.id !== undefined);
+  assert(payment.mode === 'test');
+  assert(payment.createdDatetime !== undefined);
+  assert(payment.status !== undefined);
+  assert(payment.expiryPeriod !== undefined);
+  assert(payment.amount !== undefined);
+  assert(payment.description !== undefined);
+  assert(payment.method !== undefined);
+  assert(payment.metadata !== undefined);
+  assert(payment.details !== undefined);
+  assert(payment.profileId !== undefined);
+  assert(payment.links.redirectUrl !== undefined);
+}
+
 lab.experiment('Payments API', () => {
   const Mollie = require('../index');
 
@@ -23,13 +38,25 @@ lab.experiment('Payments API', () => {
       metadata: {
         orderId: 'test'
       }
-    }, function(err, payment) {
+    }, (err, payment) => {
       assert(err === null);
       assert(payment.mode === 'test');
       assert(payment.status === 'open');
       assert(payment.metadata.orderId === 'test');
       assert(payment.description === 'My first API payment');
       assert(payment.links.redirectUrl === 'http://localhost');
+      done();
+    });
+  });
+
+  lab.test('should list payments', done => {
+    Mollie.payments.list((err, payments) => {
+      assert(err === null);
+      assert(payments.totalCount > 0);
+      assert(payments.offset === 0);
+      assert(payments.count > 0);
+      assert(Array.isArray(payments.data));
+      payments.data.forEach(validatePayment);
       done();
     });
   });
