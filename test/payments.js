92f83b30b720adb2c1a56d17e081c73f36a15417
Glenn Geenen <glenn@geenentijd.be>
2016-08-09 18:19:58 +0200

    update tests

--- test/payments.js
+++ test/payments.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Load Environment
+require('dotenv').load();
+
 const assert = require('assert');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
@@ -8,12 +11,12 @@ lab.experiment('Payments API', () => {
   const Mollie = require('../index');
 
   lab.before(done => {
-    Mollie.setApiKey('test_b93kfaAsnngIAT3NysojhYvKEJ5YbP');
+    Mollie.setApiKey(process.env.TEST_KEY);
     done();
   });
 
   lab.test('should create payment', done => {
-    Mollie.Payments.create({
+    Mollie.payments.create({
       amount: 10.00,
       description: 'My first API payment',
       redirectUrl: 'http://localhost',
@@ -22,7 +25,11 @@ lab.experiment('Payments API', () => {
       }
     }, function(err, payment) {
       assert(err === null);
-      console.log(payment);
+      assert(payment.mode === 'test');
+      assert(payment.status === 'open');
+      assert(payment.metadata.orderId === 'test');
+      assert(payment.description === 'My first API payment');
+      assert(payment.links.redirectUrl === 'http://localhost');
       done();
     });
   });
